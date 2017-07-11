987691e44a50528b6cd28ddc2f67c7ba55dea83c
FaHa <dev-faha@t-online.de>
2017-07-11 14:32:51 +0200

    added some opencl testing stuff

--- src/line_recognition.cpp
+++ src/line_recognition.cpp
@@ -1,23 +1,69 @@
 #include <drive_ros_image_recognition/line_recognition.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <iterator>
+#include <opencv2/core/ocl.hpp>
+
+#define USE_UMAT
+
+#ifdef USE_UMAT
+  #define MAT cv::UMat
+#else
+  #define MAT cv::Mat
+#endif
+
+using namespace std;
 
 namespace drive_ros_image_recognition {
 
 LineRecognition::LineRecognition( ros::NodeHandle nh ):
   it_(nh)
 {
-  img_sub_ = it_.subscribe("img_in", 10, &LineRecognition::img_callback, this);
+  img_sub_ = it_.subscribe("/camera1/image_raw", 10, &LineRecognition::img_callback, this);
+
+  if (!cv::ocl::haveOpenCL())
+  {
+      cout << "OpenCL is not avaiable..." << endl;
+      return;
+  }
+  cv::ocl::Context context;
+  if (!context.create(cv::ocl::Device::TYPE_GPU))
+  {
+      cout << "Failed creating the context..." << endl;
+      return;
+  }
+
+  // In OpenCV 3.0.0 beta, only a single device is detected.
+  cout << context.ndevices() << " GPU devices are detected." << endl;
+  for (size_t i = 0; i < context.ndevices(); i++)
+  {
+      cv::ocl::Device device = context.device(i);
+      cout << "name                 : " << device.name() << endl;
+      cout << "available            : " << device.available() << endl;
+      cout << "imageSupport         : " << device.imageSupport() << endl;
+      cout << "OpenCL_C_Version     : " << device.OpenCL_C_Version() << endl;
+      cout << endl;
+  }
+
 }
 
 LineRecognition::~LineRecognition() {
 }
 
 void LineRecognition::img_callback(const sensor_msgs::ImageConstPtr& msg) {
-  cv::Mat cv_ptr;
+  MAT cv_ptr;
   try
   {
     // todo: check if this if we have 8 or 16 greyscale images
     // hardcopies for now, might be possible to process on pointer if fast enough
+
+    // get image and convert directly to UMat
+#ifdef USE_UMAT
+    cv_ptr = cv_bridge::toCvCopy(msg, "mono16")->image.getUMat(cv::ACCESS_FAST);
+#else
     cv_ptr = cv_bridge::toCvCopy(msg, "mono16")->image;
+#endif
   }
   catch (cv_bridge::Exception& e)
   {
@@ -25,30 +71,29 @@ void LineRecognition::img_callback(const sensor_msgs::ImageConstPtr& msg) {
     return;
   }
 
-  if( !cv_ptr.data)
+  if(!cv_ptr.elemSize())
   {
     ROS_WARN("Empty image received, skipping!");
     return;
   }
 
-  // define some processing constants
+
+
+  /// define some processing constants
   int scale = 1;
   int delta = 0;
   int ddepth = CV_16S;
-  cv::Mat grad;
+  MAT grad;
 
-  cv::Mat img_blurred;
+
+  /// Blur image
+  MAT img_blurred;
   cv::GaussianBlur( cv_ptr, img_blurred, cv::Size(3,3), 0, 0, cv::BORDER_DEFAULT );
 
-  // convert to grayscale
-  cv::cvtColor( img_blurred, img_blurred, CV_BGR2GRAY );
-
-  /// Create window
-  cv::namedWindow( "Sobel", CV_WINDOW_AUTOSIZE );
 
   /// Generate grad_x and grad_y
-  cv::Mat grad_x, grad_y;
-  cv::Mat abs_grad_x, abs_grad_y;
+  MAT grad_x, abs_grad_x;
+  MAT grad_y, abs_grad_y;
 
   /// Gradient X
   cv::Sobel( img_blurred, grad_x, ddepth, 1, 0, 3, scale, delta, cv::BORDER_DEFAULT );
@@ -61,9 +106,9 @@ void LineRecognition::img_callback(const sensor_msgs::ImageConstPtr& msg) {
   /// Total Gradient (approximate)
   cv::addWeighted( abs_grad_x, 0.5, abs_grad_y, 0.5, 0, grad );
 
-  cv::imshow( "Sobel", grad );
-
-  cv::waitKey(0);
+  /// Create window
+  //cv::namedWindow( "Sobel", CV_WINDOW_AUTOSIZE );
+  //cv::imshow( "Sobel", grad );
 }
 
 } // end drive_ros_image_recognition namespace
