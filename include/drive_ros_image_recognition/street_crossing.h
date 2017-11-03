7cb17553f4893f494991e4afee51779c0bb75ee2
Simon Weigl <s.weigl@tum.de>
2017-11-03 15:34:52 +0100

    start using HoughLines for lane detection

--- include/drive_ros_image_recognition/street_crossing.h
+++ include/drive_ros_image_recognition/street_crossing.h
@@ -15,6 +15,48 @@
 
 namespace drive_ros_image_recognition {
 
+// todo: move to own file
+
+class StreetLine {
+public:
+  cv::Point p1_, p2_, mid_;
+  int vote;
+
+  StreetLine(cv::Point p1, cv::Point p2)
+    : vote(0)
+  {
+    if(p1.y < p2.y) {
+      p1_ = p1;
+      p2_ = p2;
+    } else {
+      p1_ = p2;
+      p2_ = p1;
+    }
+    mid_ = (p1 + p2) * .5;
+  }
+
+  StreetLine(cv::Vec4i c)
+    : vote(0)
+  {
+    cv::Point p1(c[0], c[1]);
+    cv::Point p2(c[2], c[3]);
+    if(p1.y < p2.y) {
+      p1_ = p1;
+      p2_ = p2;
+    } else {
+      p1_ = p2;
+      p2_ = p1;
+    }
+    mid_ = ((p1_ + p2_) * .5);
+  }
+
+  // returns the angle of the lines between 0 and PI
+  inline double getAngle() { return abs(atan2(p1_.y - p2_.y, p1_.x - p2_.x)); }
+
+  // used to sort lines ascending wrt y-coordinate
+  bool operator() (StreetLine sl1, StreetLine sl2) { return sl1.mid_.y < sl2.mid_.y; }
+};
+
 class StreetCrossingDetection {
 
 private:
@@ -24,9 +66,18 @@ private:
     float maxLineWidthMul_;
     float lineWidth_;
 
+    int cannyThreshold_;
+    int houghThresold_;
+    int houghMinLineLen_;
+    int houghMaxLineGap_;
+    int sameLineThreshold_;
+
     // variables
     std::vector<SearchLine> searchLines_;
     CvImagePtr currentImage_;
+
+//    cv::Mat warpedImg_;
+//    std::vector<StreetLine> streetLines_;
 
     int startLineCounter_;
     cv::Point lastStartLinePos_;
@@ -34,21 +85,35 @@ private:
     // communication
     image_transport::ImageTransport imageTransport_;
     image_transport::Subscriber imageSubscriber_;
+    image_transport::Subscriber warpedImageSubscriber_;
 
     ros::Publisher line_output_pub_;
+#ifdef PUBLISH_DEBUG
+    image_transport::Publisher debugImagePublisher_;
+#endif
+
+    // homography components
+      ros::Subscriber homography_params_sub_;
+      cv::Mat world2cam_;
+      cv::Mat cam2world_;
+      cv::Mat scaling_mat_;
+      cv::Mat scaling_mat_inv_;
+      cv::Size transformed_size_;
+      bool homo_received_;
 
     ros::NodeHandle nh_;
     ros::NodeHandle pnh_;
 
     void imageCallback(const sensor_msgs::ImageConstPtr& imageIn);
-
-
-#ifdef PUBLISH_DEBUG
-    image_transport::Publisher debugImagePublisher;
-#endif
+    void warpedImageCallback(const sensor_msgs::ImageConstPtr& warpedImageIn);
 
     // methods
     bool findStartline();
+    void findLane();
+
+    // this vector must always be orderer by HoughLine.mid_ (ascending)
+    std::vector<StreetLine> currentLaneMid;
+    bool onRightLine;
 
     TransformHelper transform_helper_;
     ImageOperator image_operator_;
--- src/street_crossing.cpp
+++ src/street_crossing.cpp
@@ -1,6 +1,7 @@
 #include <opencv2/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <vector>
+#include <chrono>
 #include "drive_ros_image_recognition/street_crossing.h"
 #include <pluginlib/class_list_macros.h>
 
@@ -19,7 +20,10 @@ StreetCrossingDetection::StreetCrossingDetection(const ros::NodeHandle nh, const
   , image_operator_()
   , dsrv_server_()
   , dsrv_cb_(boost::bind(&StreetCrossingDetection::reconfigureCB, this, _1, _2))
-  , startLineCounter_(0)
+  , onRightLine(true)
+//  #ifdef DRAW_DEBUG
+//  , debugImage(0, 0, CV_8UC1)
+//  #endif
 {
 }
 
@@ -27,29 +31,219 @@ StreetCrossingDetection::~StreetCrossingDetection() {
 }
 
 bool StreetCrossingDetection::init() {
+  // dynamic reconfigure
   dsrv_server_.setCallback(dsrv_cb_);
 
-  transform_helper_.init();
-  image_operator_ = ImageOperator(transform_helper_);
+  // image operations
+//  if(!transform_helper_.init())
+//    ROS_INFO_STREAM("Init transform_helper failed");
+//  image_operator_ = ImageOperator(transform_helper_);
 
+  //subscribe to camera image
   imageSubscriber_ = imageTransport_.subscribe("img_in", 10, &StreetCrossingDetection::imageCallback, this);
-  ROS_INFO_STREAM("Subscriber image transport with topic "<<imageSubscriber_.getTopic());
+  ROS_INFO_STREAM("Subscribed image transport to topic " << imageSubscriber_.getTopic());
 
-  line_output_pub_ = pnh_.advertise<drive_ros_msgs::RoadLane>("line_out",10);
+  // subscribe to warped image
+//  warpedImageSubscriber_ = imageTransport_.subscribe("warped_in", 10, &StreetCrossingDetection::warpedImageCallback, this);
+//  ROS_INFO_STREAM("Subscribed image transport to topic " << warpedImageSubscriber_.getTopic());
+
+  // publish road lane
+//  line_output_pub_ = pnh_.advertise<drive_ros_msgs::RoadLane>("line_out",10);
 
 #ifdef PUBLISH_DEBUG
-  debugImagePublisher = imageTransport_.advertise("/street_crossing/debug_image", 10);
-  ROS_INFO("Will publish debug image to /street_crossing/debug_image");
+  debugImagePublisher_ = imageTransport_.advertise("/street_crossing/debug_image", 10);
+  ROS_INFO_STREAM("Publishing debug image on " << debugImagePublisher_.getTopic());
 #endif
-#ifdef DRAW_DEBUG
-  ROS_INFO("Draw debug image");
-#endif
+
+  // common image operations
+  image_operator_.init();
+  std::string world_frame("/rear_axis_middle_ground");
+  if (!pnh_.getParam("world_frame", world_frame))
+    ROS_WARN_STREAM("Parameter 'world_frame' not found, using default: "<<world_frame);
+  std::string camera_frame("/camera_optical");
+  if (!pnh_.getParam("camera_frame", camera_frame))
+    ROS_WARN_STREAM("Parameter 'camera_frame' not found, using default: "<<camera_frame);
+  image_operator_.setCameraFrame(camera_frame);
+  image_operator_.setWorldFrame(world_frame);
+
   return true;
+}
+
+void StreetCrossingDetection::warpedImageCallback(const sensor_msgs::ImageConstPtr& warpedImageIn) {
+//  auto imgPtr = convertImageMessage(warpedImageIn);
+//  cv::cvtColor(*imgPtr, warpedImg_, CV_GRAY2RGB);
+
+//    cv::namedWindow("warped image", CV_WINDOW_NORMAL);
+//    cv::imshow("warped image", warpedImg_);
+//  ROS_INFO_STREAM("received a warped image");
 }
 
 void StreetCrossingDetection::imageCallback(const sensor_msgs::ImageConstPtr &imgIn) {
   currentImage_ = convertImageMessage(imgIn);
-  findStartline();
+
+//  findStartline();
+//  auto t_start = std::chrono::high_resolution_clock::now();
+  findLane();
+//  auto t_end = std::chrono::high_resolution_clock::now();
+//  ROS_INFO_STREAM("Cycle time: " << (std::chrono::duration<double, std::milli>(t_end-t_start).count()) << "ms");
+}
+
+void StreetCrossingDetection::findLane() {
+  cv::Mat debugImg, outputImg;
+  cv::cvtColor(*currentImage_, outputImg, CV_GRAY2RGB);
+  cv::Mat laneImg = cv::Mat::zeros(outputImg.rows, outputImg.cols, outputImg.type());
+
+  // Blur the debugImage and find edges with Canny
+  cv::GaussianBlur(*currentImage_, debugImg, cv::Size(15, 15), 0, 0);
+  cv::Canny(debugImg, debugImg, cannyThreshold_, cannyThreshold_ * 3, 3);
+
+  // Zero out the vehicle
+  int w = debugImg.cols;
+  int h = debugImg.rows;
+  cv::Point vehiclePolygon[1][4];
+  vehiclePolygon[0][0] = cv::Point(w * .35, h);
+  vehiclePolygon[0][1] = cv::Point(w * .65, h);
+  vehiclePolygon[0][2] = cv::Point(w * .65, h * .8);
+  vehiclePolygon[0][3] = cv::Point(w * .35, h * .8);
+  const cv::Point* ppt[1] = { vehiclePolygon[0] };
+  int npt[] = { 4 };
+  cv::fillPoly(debugImg, ppt, npt, 1, cv::Scalar(), cv::LINE_8);
+
+  // If we dont have a lane from last step (or this is the first search), use straight line forward
+  if(currentLaneMid.empty()) {
+    currentLaneMid.push_back(StreetLine(cv::Point(w/2, h), cv::Point(w/2, 0)));
+  }
+
+  // Display the polygon which will be zeroed out
+//  cv::line(outputImg, vehiclePolygon[0][0], vehiclePolygon[0][1], cv::Scalar(255), 2, cv::LINE_AA);
+//  cv::line(outputImg, vehiclePolygon[0][1], vehiclePolygon[0][2], cv::Scalar(255), 2, cv::LINE_AA);
+//  cv::line(outputImg, vehiclePolygon[0][2], vehiclePolygon[0][3], cv::Scalar(255), 2, cv::LINE_AA);
+//  cv::line(outputImg, vehiclePolygon[0][3], vehiclePolygon[0][0], cv::Scalar(255), 2, cv::LINE_AA);
+
+  // Get houghlines and draw them on the output image
+  std::vector<cv::Vec4i> hLinePoints;
+  std::vector<StreetLine> leftLines, midLines, rightLines;
+  cv::HoughLinesP(debugImg, hLinePoints, 1, CV_PI / 180, houghThresold_, houghMinLineLen_, houghMaxLineGap_);
+
+  // Assign the hough-lines returned by HoughLinesP to left, mid, or right line
+  for(size_t i = 0; i < hLinePoints.size(); i++) {
+    auto l = StreetLine(hLinePoints[i]);
+
+    // todo: find current line segment
+    StreetLine currentSegment = currentLaneMid.at(0);
+
+    if(currentSegment.mid_.x < l.mid_.x) {
+      rightLines.push_back(l);
+    } else if((currentSegment.mid_.x - (w * .15)) > l.mid_.x) {
+      leftLines.push_back(l);
+    } else {
+      midLines.push_back(l);
+    }
+
+
+    // (0,0)                (1280,344)
+    // (-0.0506457,3.06453) (6.85171,-0.989774)
+    cv::Point2f wPt1, wPt2;
+//    image_operator_.imageToWorld(l.p1_, wPt1);
+//    image_operator_.imageToWorld(l.p2_, wPt2);
+    image_operator_.imageToWorld(cv::Point(0, 0), wPt1);
+    image_operator_.imageToWorld(cv::Point(w, h), wPt2);
+    cv::line(laneImg, wPt1, wPt2, cv::Scalar(255), 2, cv::LINE_AA);
+    ROS_INFO_STREAM("Transformed (0,0) to (" << wPt1.x << "," << wPt1.y <<")");
+    ROS_INFO_STREAM("Transformed (" << w << "," << h << " to (" << wPt2.x << "," << wPt2.y << ")");
+  }
+
+
+  cv::line(outputImg, cv::Point(w * .15, h), cv::Point(w * .45, 0), cv::Scalar(0, 255, 255), 2, cv::LINE_AA);
+
+  for(auto l : leftLines)
+    cv::line(outputImg, l.p1_, l.p2_, cv::Scalar(255), 2, cv::LINE_AA);
+  for(auto l : midLines)
+    cv::line(outputImg, l.p1_, l.p2_, cv::Scalar(0, 255), 2, cv::LINE_AA);
+  for(auto l : rightLines)
+    cv::line(outputImg, l.p1_, l.p2_, cv::Scalar(0, 0, 255), 2, cv::LINE_AA);
+
+//    auto a = l.getAngle();
+//    if((a > (CV_PI/6)) && (a < 2.6)) {
+//      // lane line
+//      for(size_t j = 0; j < laneLines.size(); j++) {
+//        auto d = abs(l.mid_.x - laneLines.at(j).mid_.x);
+//        if(d < sameLineThreshold_) {
+//          laneLines.at(j).vote++;
+//          l.vote++;
+//        }
+//      }
+//      laneLines.push_back(l);
+//    } else {
+//      // stop line
+//      for(size_t j = 0; j < stopLines.size(); j++) {
+//        auto d = abs(l.mid_.y - stopLines.at(j).mid_.y);
+//        if(d < sameLineThreshold_) {
+//          stopLines.at(j).vote++;
+//          l.vote++;
+//        }
+//      }
+//      stopLines.push_back(l);
+//    }
+
+//    for(auto l : laneLines) {
+//      if(l.vote > 0)
+//        cv::line(outputImg, l.p1_, l.p2_, cv::Scalar(0,255), 2, cv::LINE_AA);
+//      else
+//        cv::line(outputImg, l.p1_, l.p2_, cv::Scalar(0, 0, 255), 2, cv::LINE_AA);
+//    }
+
+//    for(auto l : stopLines) {
+//      if(l.vote > 0)
+//        cv::line(outputImg, l.p1_, l.p2_, cv::Scalar(255), 2, cv::LINE_AA);
+//      else
+//        cv::line(outputImg, l.p1_, l.p2_, cv::Scalar(0, 0, 255), 2, cv::LINE_AA);
+//    }
+
+//      bool added = false;
+//      for(size_t j = 0; j < myLines.size(); j++) {
+//        // todo: use world distance here with linewidth
+//        auto d = cv::norm(l.mid_ - myLines.at(j).mid_);
+//        if(d < sameLineThreshold_) {
+//          myLines.at(j).vote++;
+//          added = true;
+//        }
+//      }
+//      if(!added) {
+//        myLines.push_back(l);
+//      }
+//  }
+
+//  int linesDrawn = 0;
+//  for(size_t i = 0; i < myLines.size(); i++) {
+//    auto l = myLines.at(i);
+//    auto a = l.getAngle();
+//    if(l.vote > 0) {
+//      linesDrawn++;
+//      if((a > (CV_PI/6)) && (a < 2.6)) {
+//        // lane marking
+//        cv::line(outputImg, l.p1_, l.p2_, cv::Scalar(0,255), 2, cv::LINE_AA);
+//      } else {
+//        // stop or start line
+//        cv::line(outputImg, l.p1_, l.p2_, cv::Scalar(255), 2, cv::LINE_AA);
+//      }
+//    } else {
+//      // discarded line due to votes
+//      cv::line(outputImg, l.p1_, l.p2_, cv::Scalar(0, 0, 255), 2, cv::LINE_AA);
+//    }
+//  }
+
+//  ROS_INFO_STREAM("out of " << hLinePoints.size() << " hough lines, " << linesDrawn << " survived");
+
+#ifdef PUBLISH_DEBUG
+  debugImagePublisher_.publish(cv_bridge::CvImage(std_msgs::Header(), sensor_msgs::image_encodings::RGB8, outputImg).toImageMsg());
+#endif
+
+  cv::namedWindow("debug image", CV_WINDOW_NORMAL);
+  cv::imshow("debug image", outputImg);
+  cv::namedWindow("lane image", CV_WINDOW_NORMAL);
+  cv::imshow("lane image", laneImg);
+  cv::waitKey(1);
 }
 
 bool StreetCrossingDetection::findStartline() {
@@ -100,7 +294,7 @@ bool StreetCrossingDetection::findStartline() {
   cv::waitKey(1);
 #endif
 #ifdef PUBLISH_DEBUG
-  debugImagePublisher.publish(cv_bridge::CvImage(std_msgs::Header(), sensor_msgs::image_encodings::TYPE_8UC1, debug_image).toImageMsg());
+//  debugImagePublisher.publish(cv_bridge::CvImage(std_msgs::Header(), sensor_msgs::image_encodings::TYPE_8UC1, debugImage).toImageMsg());
 #endif
 
   // test array of lines
@@ -113,6 +307,12 @@ void StreetCrossingDetection::reconfigureCB(drive_ros_image_recognition::LineDet
   minLineWidthMul_ = config.minLineWidthMul;
   maxLineWidthMul_ = config.maxLineWidthMul;
   lineWidth_ = config.lineWidth;
+
+  cannyThreshold_ = config.cannyThreshold;
+  houghThresold_ = config.houghThreshold;
+  houghMinLineLen_ = config.houghMinLineLen;
+  houghMaxLineGap_ = config.houghMaxLineGap;
+  sameLineThreshold_ = config.sameLineThreshold;
 }
 
 void StreetCrossingDetectionNodelet::onInit() {
